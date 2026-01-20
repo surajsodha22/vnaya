c2e240d63ca0f400a17e0926fdee5d5ee517032f
surajcybdeer <surajcybdeer@gmail.com>
2026-01-20 13:02:16 +0530

    add favicon

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
+import faviconUrl from './assets/img/favicon.png';
+
+// Ensure the favicon uses our project asset
+function applyFavicon(href: string) {
+  let link = document.querySelector<HTMLLinkElement>("link[rel='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+  }
+  link.href = href;
+}
+applyFavicon(faviconUrl);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
